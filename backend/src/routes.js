e3029b87d136828d2448e39056ec3c9dfc414d83
bruno-gois <brunovgois@gmail.com>
2019-10-23 23:27:34 -0300

    middleware de autenticação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import StudentsController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/students', StudentsController.store);
 routes.post('/session', SessionController.store);
 
+routes.use(authMiddleware);
+routes.post('/students', StudentsController.store);
 
 export default routes;
