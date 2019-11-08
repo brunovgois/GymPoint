3c74c87e0a024645c5d6576c705ff183f3735e5a
bruno-gois <brunovgois@gmail.com>
2019-11-08 20:00:33 -0300

    model e migration de matriculas

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,11 +3,11 @@ import Sequelize from "sequelize";
 import User from "../app/models/User";
 import Students from "../app/models/Student";
 import Plan from "../app/models/Plan";
+import Enrollment from "../app/models/Enrollment";
 
 import databaseConfig from "../config/database";
 
-// array de models
-const models = [User, Students, Plan];
+const models = [User, Students, Plan, Enrollment];
 
 class Database {
   constructor() {
@@ -16,7 +16,6 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    //chama o metodo init de cada model
     models.map(model => model.init(this.connection));
   }
 }
